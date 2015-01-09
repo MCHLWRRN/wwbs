5766be4bd2d0104fbd9d1d13e57a22bf0ba67038
MCHLWRRN <me@mchlwrrn.com>
2015-01-08 16:14:48 -0800

    Added data validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :lyric, :presence => true, :length => {:maximum => 140, :minimum => 3}
+	validates :song, :presence => true, :length => { :maximum => 50, :minimum => 3}
 end
